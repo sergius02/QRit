d00e5ec2ddbab5f833d86dae88694fbd23cc044f
Sergio Fernández Celorio <scelorio1990@gmail.com>
2020-11-18 13:44:43 +0100

    🐛 Detects WSL during installation

--- meson/post_install.py
+++ meson/post_install.py
@@ -2,6 +2,7 @@
 
 from os import path, environ
 import subprocess
+import platform
 
 prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
 schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
@@ -11,7 +12,10 @@ desktop_database_dir = path.join(datadir, 'applications')
 if not environ.get('DESTDIR'):
     print('Compiling gsettings schemas…')
     subprocess.call(['glib-compile-schemas', schemadir])
-    print('Updating desktop database…')
-    subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
-    print('Updating icon cache…')
-    subprocess.call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+
+    on_wsl = "microsoft" in platform.uname()[3].lower()
+    if not on_wsl:
+        print('Updating desktop database…')
+        subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
+        print('Updating icon cache…')
+        subprocess.call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
